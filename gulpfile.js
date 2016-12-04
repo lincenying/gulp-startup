8afa9223191dd71955fb018e7d589cc54dfdb39f
lincenying <lincenying@qq.com>
2016-12-05 00:07:53 +0800

    添加sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,40 @@
 
 var gulp = require('gulp'),
     less = require('gulp-less'),
-    connect = require('gulp-connect'),
     postcss = require('gulp-postcss'),
+    sass = require('gulp-sass'),
+    connect = require('gulp-connect'),
     browserslist = require('browserslist'),
     LessPluginAutoPrefix = require('less-plugin-autoprefix'),
+    autoprefixer = require('autoprefixer'),
     salad = require('postcss-salad')
 
-var basedir = 'postcss/',
+var basedir = 'E:/web/webpack/mmf-blog-v2-vue2/html/',
     browsers = browserslist('last 2 version, > 0.1%'),
-    autoprefix = new LessPluginAutoPrefix({
+    lessAutoprefix = new LessPluginAutoPrefix({
         browsers: browsers
     })
 
 gulp.task('start_less', ['auto_server', 'auto_task_less'])
 gulp.task('start_postcss', ['auto_server', 'auto_task_postcss'])
+gulp.task('start_scss', ['auto_server', 'auto_task_scss'])
 
 // 自动任务
+// less 相关任务
 gulp.task('auto_task_less', function() {
     gulp.watch(basedir + 'less/*.less', ['auto_less'])
     gulp.watch(basedir + '*.html', ['auto_refresh_from_html'])
     gulp.watch(basedir + 'css/*.css', ['auto_refresh_from_css'])
 })
+// postcss 相关任务
 gulp.task('auto_task_postcss', function() {
     gulp.watch(basedir + 'original/*.css', ['auto_postcss'])
+    gulp.watch(basedir + '*.html', ['auto_refresh_from_html'])
+    gulp.watch(basedir + 'css/*.css', ['auto_refresh_from_css'])
+})
+// scss 相关任务
+gulp.task('auto_task_scss', function() {
+    gulp.watch(basedir + 'scss/*.scss', ['auto_scss'])
     gulp.watch(basedir + '*.html', ['auto_refresh_from_html'])
     gulp.watch(basedir + 'css/*.css', ['auto_refresh_from_css'])
 })
@@ -32,7 +43,7 @@ gulp.task('auto_task_postcss', function() {
 // 编译less文件
 gulp.task('auto_less', function() {
     gulp.src(basedir + 'less/*.less')
-        .pipe(less({ plugins: [autoprefix] }))
+        .pipe(less({ plugins: [lessAutoprefix] }))
         .pipe(gulp.dest(basedir + 'css/'))
 })
 
@@ -42,6 +53,14 @@ gulp.task('auto_postcss', function() {
         .pipe(postcss([
             salad({browsers: browsers})
         ]))
+        .pipe(gulp.dest(basedir + 'css/'))
+})
+
+// 编译scss文件
+gulp.task('auto_scss', function() {
+    gulp.src(basedir + 'scss/style.scss')
+        .pipe(sass())
+        .pipe(autoprefixer(browsers))
         .pipe(gulp.dest(basedir + 'css/'))
 })
 
