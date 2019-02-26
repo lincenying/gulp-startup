8c125fc819fc896e301b91e4a59776de1721590a
林岑影 <lincenying@qq.com>
2019-02-26 20:29:42 +0800

    up

--- gulpfile.js
+++ gulpfile.js
@@ -18,55 +18,61 @@ var basedir = 'less/', // <= 修改该路径
     })
 
 gulp.task('auto_server', function() {
-    connect.server({
-        root: basedir,
-        port: 9092,
-        livereload: true,
-        middleware: function(connect, opt) {
-            return [
-                proxy('/base_api',  {
-                    target: 'http://127.0.0.1:3000',
-                    changeOrigin:true
-                }),
-                proxy('/shike_api', {
-                    target: 'http://127.0.0.1:3000',
-                    changeOrigin:true
-                })
-            ]
-        }
+    return new Promise(function(resolve) {
+        connect.server({
+            root: basedir,
+            port: 9092,
+            livereload: true,
+            middleware: function(connect, opt) {
+                return [
+                    proxy('/base_api',  {
+                        target: 'http://127.0.0.1:3000',
+                        changeOrigin:true
+                    }),
+                    proxy('/shike_api', {
+                        target: 'http://127.0.0.1:3000',
+                        changeOrigin:true
+                    })
+                ]
+            }
+        })
+        resolve()
     })
 })
 
 // 自动任务
 // less 相关任务
-gulp.task('auto_task_less', function() {
+gulp.task('auto_task_less', function(done) {
     gulp.watch(basedir + 'less/**/*.less', gulp.series('auto_less'))
     gulp.watch(basedir + '*.html', gulp.series('auto_refresh_from_html'))
     gulp.watch(basedir + 'css/*.css', gulp.series('auto_refresh_from_css'))
+    done()
 })
 // postcss 相关任务
-gulp.task('auto_task_postcss', function() {
+gulp.task('auto_task_postcss', function(done) {
     gulp.watch(basedir + 'original/**/*.css', gulp.series('auto_postcss'))
     gulp.watch(basedir + '*.html', gulp.series('auto_refresh_from_html'))
     gulp.watch(basedir + 'css/*.css', gulp.series('auto_refresh_from_css'))
+    done()
 })
 // scss 相关任务
-gulp.task('auto_task_scss', function() {
+gulp.task('auto_task_scss', function(done) {
     gulp.watch(basedir + 'scss/**/*.scss', gulp.series('auto_scss'))
     gulp.watch(basedir + '*.html', gulp.series('auto_refresh_from_html'))
     gulp.watch(basedir + 'css/*.css', gulp.series('auto_refresh_from_css'))
+    done()
 })
 
 // 编译less文件
 gulp.task('auto_less', function() {
-    gulp.src([basedir + 'less/**/*.less', '!' + basedir + 'less/**/_*.less'])
+    return gulp.src([basedir + 'less/**/*.less', '!' + basedir + 'less/**/_*.less'])
         .pipe(less({ plugins: [lessAutoprefix] }))
         .pipe(gulp.dest(basedir + 'css/'))
 })
 
 // 编辑postcss文件
 gulp.task('auto_postcss', function() {
-    gulp.src([basedir + 'original/**/*.css', '!' + basedir + 'original/**/_*.css'])
+    return gulp.src([basedir + 'original/**/*.css', '!' + basedir + 'original/**/_*.css'])
         .pipe(postcss([
             salad({browsers: browsers})
         ]))
@@ -75,18 +81,18 @@ gulp.task('auto_postcss', function() {
 
 // 编译scss文件
 gulp.task('auto_scss', function() {
-    gulp.src([basedir + 'scss/**/*.scss', '!' + basedir + 'scss/**/_*.scss'])
+    return gulp.src([basedir + 'scss/**/*.scss', '!' + basedir + 'scss/**/_*.scss'])
         .pipe(sass())
         .pipe(autoprefixer(browsers))
         .pipe(gulp.dest(basedir + 'css/'))
 })
 
 gulp.task('auto_refresh_from_html', function() {
-    gulp.src(basedir + '*.html')
+    return gulp.src(basedir + '*.html')
         .pipe(connect.reload())
 })
 gulp.task('auto_refresh_from_css', function() {
-    gulp.src(basedir + 'css/*.css')
+    return gulp.src(basedir + 'css/*.css')
         .pipe(connect.reload())
 })
 
