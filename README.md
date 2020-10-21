3b9369ea5aa2832983180e85240c906f7dfa5a9f
林岑影 <lincenying@qq.com>
2020-10-21 15:26:23 +0800

    on 2020-10-21 15:26:23

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # gulp-startup
 
-切图必备, 自动编译less/postcss/scss, 自动添加css3前缀, 自动刷新浏览器
+切图必备, 自动编译less/postcss/scss, 自动添加css3前缀, h5模式时, 自动将px转成rem, 自动刷新浏览器
 
 ```bash
 # 安装依赖
 npm install  or  yarn
 
 # less 模式
-npm run less
+npm run less  or  yarn less
 
 ### 或者
 
 gulp start_less
 
 # scss 模式
-npm run scss
+npm run scss  or  yarn scss
 
 ### 或者
 
@@ -22,7 +22,7 @@ gulp start_scss
 
 # postcss-salad 模式
 
-npm run postcss
+npm run postcss  or  yarn postcss
 
 ### 或者
 
@@ -31,4 +31,7 @@ gulp start_postcss
 
 打开 http://localhost:9092
 
-自己建立切图文件夹后, 记得修改`gulpfile.js`的`basedir`
+自己建立切图文件夹后, 记得修改`gulpfile.js`的`basedir` 和 `siteType`
+
+siteType = h5 时:
+会将px单位自动转成rem, 默认比例100, 即设计稿宽度为750px, 书写时, css只需要写成设计稿的真实尺寸即可
--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,35 @@
-/* eslint-disable */
+const gulp = require('gulp')
+const less = require('gulp-less')
+const postcss = require('gulp-postcss')
+const sass = require('gulp-sass')
+const connect = require('gulp-connect')
+const proxy = require('http-proxy-middleware')
+// const LessPluginAutoPrefix = require('less-plugin-autoprefix')
+const autoprefixer = require('autoprefixer')
+const salad = require('postcss-salad')
+const pxtorem = require('postcss-pxtorem')
 
-var gulp = require('gulp'),
-    less = require('gulp-less'),
-    postcss = require('gulp-postcss'),
-    sass = require('gulp-sass'),
-    connect = require('gulp-connect'),
-    proxy = require('http-proxy-middleware'),
-    LessPluginAutoPrefix = require('less-plugin-autoprefix'),
-    autoprefixer = require('autoprefixer'),
-    salad = require('postcss-salad')
+const browsers = ['> 1%', 'last 3 versions', 'not ie <= 8']
 
-var browsers = ['last 3 versions']
-var basedir = 'postcss/', // <= 修改该路径
-    lessAutoprefix = new LessPluginAutoPrefix({
-        browsers: browsers
-    })
+const basedir = 'less/' // <= 修改该路径
+const siteType = 'h5' // <= 网站类型:  h5 或者  web
 
-gulp.task('auto_server', function() {
-    return new Promise(function(resolve) {
+gulp.task('auto_server', function () {
+    return new Promise(function (resolve) {
         connect.server({
             root: basedir,
             port: 9092,
             livereload: true,
-            middleware: function(connect, opt) {
+            // eslint-disable-next-line no-unused-vars
+            middleware(connect, opt) {
                 return [
-                    proxy.createProxyMiddleware('/base_api',  {
+                    proxy.createProxyMiddleware('/base_api', {
                         target: 'http://127.0.0.1:3000',
-                        changeOrigin:true
+                        changeOrigin: true
                     }),
                     proxy.createProxyMiddleware('/shike_api', {
                         target: 'http://127.0.0.1:3000',
-                        changeOrigin:true
+                        changeOrigin: true
                     })
                 ]
             }
@@ -41,21 +40,21 @@ gulp.task('auto_server', function() {
 
 // 自动任务
 // less 相关任务
-gulp.task('auto_task_less', function(done) {
+gulp.task('auto_task_less', function (done) {
     gulp.watch(basedir + 'less/**/*.less', gulp.series('auto_less'))
     gulp.watch(basedir + '*.html', gulp.series('auto_refresh_from_html'))
     gulp.watch(basedir + 'css/*.css', gulp.series('auto_refresh_from_css'))
     done()
 })
 // postcss 相关任务
-gulp.task('auto_task_postcss', function(done) {
+gulp.task('auto_task_postcss', function (done) {
     gulp.watch(basedir + 'original/**/*.css', gulp.series('auto_postcss'))
     gulp.watch(basedir + '*.html', gulp.series('auto_refresh_from_html'))
     gulp.watch(basedir + 'css/*.css', gulp.series('auto_refresh_from_css'))
     done()
 })
 // scss 相关任务
-gulp.task('auto_task_scss', function(done) {
+gulp.task('auto_task_scss', function (done) {
     gulp.watch(basedir + 'scss/**/*.scss', gulp.series('auto_scss'))
     gulp.watch(basedir + '*.html', gulp.series('auto_refresh_from_html'))
     gulp.watch(basedir + 'css/*.css', gulp.series('auto_refresh_from_css'))
@@ -63,38 +62,74 @@ gulp.task('auto_task_scss', function(done) {
 })
 
 // 编译less文件
-gulp.task('auto_less', function() {
-    return gulp.src([basedir + 'less/**/*.less', '!' + basedir + 'less/**/_*.less'])
-        .pipe(less({ plugins: [lessAutoprefix] }))
+gulp.task('auto_less', function () {
+    const processors = [
+        autoprefixer({
+            browsers
+        })
+    ]
+    if (siteType === 'h5') {
+        processors.push(
+            pxtorem({
+                rootValue: 100,
+                unitPrecision: 6, // 转换后的精度，即小数点位数
+                propList: ['*'], // 指定可以转换的css属性，*代表全部css属性
+                selectorBlackList: ['van-circle__layer'], // 指定不转换为视窗单位的类名
+                mediaQuery: true, // 是否在媒体查询的css代码中也进行转换，默认false
+                minPixelValue: 1, // 默认值1，小于或等于1px则不进行转换
+                exclude: [/node_modules/] // 设置忽略文件，用正则做目录名匹配
+            })
+        )
+    }
+    return gulp
+        .src([basedir + 'less/**/*.less', '!' + basedir + 'less/**/_*.less'])
+        .pipe(less())
+        .pipe(postcss(processors))
         .pipe(gulp.dest(basedir + 'css/'))
 })
 
 // 编辑postcss文件
-gulp.task('auto_postcss', function() {
-    return gulp.src([basedir + 'original/**/*.css', '!' + basedir + 'original/**/_*.css'])
-        .pipe(postcss([
-            salad({browsers: browsers})
-        ]))
+gulp.task('auto_postcss', function () {
+    return gulp
+        .src([basedir + 'original/**/*.css', '!' + basedir + 'original/**/_*.css'])
+        .pipe(postcss([salad({ browsers })]))
         .pipe(gulp.dest(basedir + 'css/'))
 })
 
 // 编译scss文件
-gulp.task('auto_scss', function() {
-    return gulp.src([basedir + 'scss/**/*.scss', '!' + basedir + 'scss/**/_*.scss'])
+gulp.task('auto_scss', function () {
+    const processors = [
+        autoprefixer({
+            browsers
+        })
+    ]
+    if (siteType === 'h5') {
+        processors.push(
+            pxtorem({
+                rootValue: 100,
+                unitPrecision: 6, // 转换后的精度，即小数点位数
+                propList: ['*'], // 指定可以转换的css属性，*代表全部css属性
+                selectorBlackList: ['van-circle__layer'], // 指定不转换为视窗单位的类名
+                mediaQuery: true, // 是否在媒体查询的css代码中也进行转换，默认false
+                minPixelValue: 1, // 默认值1，小于或等于1px则不进行转换
+                exclude: [/node_modules/] // 设置忽略文件，用正则做目录名匹配
+            })
+        )
+    }
+    return gulp
+        .src([basedir + 'scss/**/*.scss', '!' + basedir + 'scss/**/_*.scss'])
         .pipe(sass())
-        .pipe(postcss([ autoprefixer() ]))
+        .pipe(postcss(processors))
         .pipe(gulp.dest(basedir + 'css/'))
 })
 
-gulp.task('auto_refresh_from_html', function() {
-    return gulp.src(basedir + '*.html')
-        .pipe(connect.reload())
+gulp.task('auto_refresh_from_html', function () {
+    return gulp.src(basedir + '*.html').pipe(connect.reload())
 })
-gulp.task('auto_refresh_from_css', function() {
-    return gulp.src(basedir + 'css/*.css')
-        .pipe(connect.reload())
+gulp.task('auto_refresh_from_css', function () {
+    return gulp.src(basedir + 'css/*.css').pipe(connect.reload())
 })
 
 gulp.task('start_less', gulp.parallel('auto_server', 'auto_task_less'))
-gulp.task('start_postcss', gulp.parallel('auto_server', 'auto_task_postcss'))
 gulp.task('start_scss', gulp.parallel('auto_server', 'auto_task_scss'))
+gulp.task('start_postcss', gulp.parallel('auto_server', 'auto_task_postcss'))
